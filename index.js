7e9045d709d15365ed226349a5b678398ac6214e
Adam Suchi Hafizullah <adamsuchihafizullah@gmail.com>
2024-07-12 06:57:53 +0700

    update: add body parser and cors

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
-
 const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const port = 3000;
 
