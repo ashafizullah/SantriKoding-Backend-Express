142b6701c5bc105d828289580fe7b147d1e2cec1
Adam Suchi Hafizullah <adamsuchihafizullah@gmail.com>
2024-07-13 00:26:01 +0700

    update: user register function

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const router = require('./routes');
+
+const app = express();
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,6 +14,8 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.use('/api', router)
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
